a837e06b6bdc16b6ed8e502180142e93cb389a3b
othmane106 <121968992+othmane106@users.noreply.github.com>
2023-05-11 19:52:12 +0100

    Add files via upload

---
+++ index.html/README.md
@@ -0,0 +1,20 @@
+# Stock-Price
+This project was designed as an assignment for NeogCamp LevelZero(Mark-14).
+
+This app is a Stock-Price app using HTML,CSS and JavaScript.This app helps the user to calculate whether he/she has made profit/loss in Stock Market.To calculate if we take three 
+input from the user(initial price,quantity and current price) using these three value be calculate the Total Profit/Loss the user has made and we also tell the percentage for the 
+same.
+Curious to know how is your Porfolio is doing.So what are you waiting for,go on the below link and find profit/loss.
+
+This project has been deployed by using Netlify.To enjoy the Demo Project you can follow the Deployed Demo Link.
+
+
+
+Stock-Price يستخدم HTML و CSS و JavaScript. يساعد هذا التطبيق المستخدم على حساب ما إذا كان قد حقق ربحًا / خسارة في سوق الأوراق المالية.
+المدخلات من المستخدم (السعر الأولي والكمية والسعر الحالي) باستخدام هذه القيم الثلاثة يتم حساب إجمالي الربح / الخسارة الذي حققه المستخدم ونخبر أيضًا النسبة المئوية لـ
+نفس.
+فضولي لمعرفة كيف يعمل Porfolio الخاص بك. فما الذي تنتظره ، انتقل إلى الرابط أدناه وابحث عن الربح / الخسارة.
+
+تم نشر هذا المشروع باستخدام Netlify. للاستمتاع بالمشروع التجريبي ، يمكنك اتباع الرابط التجريبي المنشور.
+
+[Stock-Price-Calculator](https://ultronjs-stockprofitorlosscal.netlify.app/ "Stock-Price")
---
+++ index.html/app.js
@@ -0,0 +1,92 @@
+var initialPrice = document.querySelector("#initial_price");
+var stockQuantity = document.querySelector("#stock_quantity");
+var currentPrice = document.querySelector("#current_price");
+var btnCalculate = document.querySelector("#btn_calculate");
+var outboxContainer = document.querySelector("#outbox_container");
+var body = document.querySelector("body");
+var para = document.querySelector("p") ;
+
+
+btnCalculate.addEventListener("click", clickHandler);
+
+
+function clickHandler(){
+  if(initialPrice.value === "" || stockQuantity.value === "" || currentPrice.value === ""){
+    console.log(initialPrice.value,stockQuantity.value,currentPrice.value)
+  alert("Please Enter all the information required to Calculate the Profit or Loss")
+  }
+  else if (Number(stockQuantity.value) === 0) {
+    alert("Stock Quantity can't be zero");
+  }else {
+   calculateProfitOrLoss(Number(initialPrice.value),Number(stockQuantity.value),Number(currentPrice.value));
+  }
+}
+
+function calculateProfitOrLoss(initial,quantity,current){
+  console.log(initial,quantity,current)
+  if(current > initial){
+    var profitPerShare = current - initial;
+    var totalProfit = quantity * profitPerShare;
+    var profitPercentage = (profitPerShare / initial) * 100;
+    showMessage(`Hey the profit is ${totalProfit} and the percent is ${profitPercentage.toFixed(2)}%`); 
+    showImage("PROFIT");
+    changeTheme("PROFIT");
+  }else if(current < initial){
+    var lossPerShare = initial - current;
+    var totalLoss = quantity * lossPerShare;
+    var lossPercentage = (lossPerShare / initial) * 100;
+    showMessage(`Hey the loss is ${totalLoss} and the percent is ${lossPercentage.toFixed(2)}%`);
+    showImage("LOSS")
+    changeTheme("LOSS");
+  }else{
+    showMessage("The old Chinese proverb springs to mind - No pain, no gain.");
+    showImage("NOGAIN");
+    changeTheme("NOGAIN");
+  }
+}
+
+function showMessage(message){
+  outboxContainer.innerHTML = message
+}
+
+function showImage(status){
+  var img = document.createElement("img");
+  img.setAttribute("id", "result_img");
+  switch (status) {
+    case "PROFIT":
+      img.setAttribute("src", "./images/profit.png");
+      outboxContainer.appendChild(img)
+      break;
+    case "LOSS":
+      img.setAttribute("src", "./images/loss.png");
+      outboxContainer.appendChild(img);
+      break;
+
+    case "NOGAIN":
+      img.setAttribute("src", "./images/nogain.png");
+      outboxContainer.appendChild(img);
+      break;
+    default:
+      break;
+  }
+}
+
+function changeTheme(status){
+  switch (status) {
+    case "PROFIT":
+      body.style.backgroundColor = "green";
+      para.style.color = "white";
+      break;
+    case "LOSS":
+      body.style.backgroundColor = "red";
+      para.style.color = "white";
+      break;
+
+    case "NOGAIN":
+      body.style.backgroundColor = "#FFF49A";
+      para.style.color = "white";
+      break;
+    default:
+      break;
+  }
+}
